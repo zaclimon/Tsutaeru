87734311720755870e6edd275bef5a6ac3f4e2a5
zaclimon <isaacpateau05@gmail.com>
2017-06-25 17:04:17 -0400

    AuthActivityTv: Finish the activity on the last GuidedStepFragment
    If we're on the "Connected" phase of the process, let's be sure
    that it's not possible to come back to previous steps and finish
    accordingly the activity.

--- app/src/main/java/com/zaclimon/aceiptv/auth/AuthActivityTv.java
+++ app/src/main/java/com/zaclimon/aceiptv/auth/AuthActivityTv.java
@@ -14,6 +14,7 @@ import com.zaclimon.aceiptv.util.Constants;
 import com.zaclimon.aceiptv.util.Utilities;
 
 import static android.R.attr.password;
+import static android.support.v17.leanback.app.GuidedStepFragment.getCurrentGuidedStepFragment;
 
 /**
  * Created by isaac on 17-06-07.
@@ -28,6 +29,19 @@ public class AuthActivityTv extends Activity {
         if (Utilities.isTvMode(this)) {
             GuidedStepFragment.addAsRoot(this, new WelcomeStepAuthGuidedFragment(), android.R.id.content);
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Similar to the Leanback showcase, exit the Activity if we're connected.
+        GuidedStepFragment guidedStepFragment = GuidedStepFragment.getCurrentGuidedStepFragment(getFragmentManager());
+
+        if (guidedStepFragment != null && guidedStepFragment instanceof ConnectedStepAuthGuidedFragment) {
+            finish();
+        } else {
+            super.onBackPressed();
+        }
+
     }
 
 }
