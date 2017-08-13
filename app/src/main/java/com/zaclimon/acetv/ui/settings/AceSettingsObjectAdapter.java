4f5800ab63e42ea626b2725bd9f428068d78272c
zaclimon <isaacpateau05@gmail.com>
2017-08-13 19:20:26 -0400

    AceSettingsObjectAdapter: Use the shorter title for video fit

--- app/src/main/java/com/zaclimon/acetv/ui/settings/AceSettingsObjectAdapter.java
+++ app/src/main/java/com/zaclimon/acetv/ui/settings/AceSettingsObjectAdapter.java
@@ -16,7 +16,7 @@ public class AceSettingsObjectAdapter extends ProviderSettingsObjectAdapter {
         addSection(R.string.stream_type, R.drawable.ic_view_stream);
         addSection(R.string.channel_logo_title_short, R.drawable.ic_channel_logo);
         addSection(R.string.force_epg_sync_title_short, R.drawable.ic_sync_epg);
-        addSection(R.string.force_video_fit_title, R.drawable.ic_force_video_fit);
+        addSection(R.string.force_video_fit_title_short, R.drawable.ic_force_video_fit);
         addSection(R.string.channel_region_title, R.drawable.ic_channel_region);
         addSection(R.string.change_user_title, R.drawable.ic_change_user);
         addSection(R.string.clear_cache_title_short, R.drawable.ic_clear_cache);
