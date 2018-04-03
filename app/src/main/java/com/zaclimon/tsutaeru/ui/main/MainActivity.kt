c7cb0e34ee10ccba676edad2513910d7a388fd0f
zaclimon <isaacpateau05@gmail.com>
2018-04-03 18:52:34 -0400

    ui: Convert main package to Kotlin

---
+++ app/src/main/java/com/zaclimon/tsutaeru/ui/main/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.zaclimon.tsutaeru.ui.main
+
+import android.content.Intent
+import android.media.tv.TvContract
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import android.widget.Toast
+import com.zaclimon.tsutaeru.R
+import com.zaclimon.tsutaeru.ui.auth.AuthActivityTv
+import com.zaclimon.tsutaeru.util.ActivityUtil
+import com.zaclimon.tsutaeru.util.Constants
+
+/**
+ * First activity upon opening the application. Decides of the layouts to use and if
+ * a sign-in is required.
+ *
+ * @author zaclimon
+ */
+class MainActivity : FragmentActivity() {
+
+    private val authRequest = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (ActivityUtil.isUsernamePasswordEmpty(this) && ActivityUtil.isTvMode(this)) {
+            val intent = Intent(this, AuthActivityTv::class.java)
+            val inputId = TvContract.buildInputId(Constants.TV_INPUT_SERVICE_COMPONENT)
+
+            // Delete all channels in case of where the user data has been cleared.
+            contentResolver.delete(TvContract.buildChannelsUriForInput(inputId), null, null)
+            startActivityForResult(intent, authRequest)
+        } else {
+            configureLayout()
+        }
+    }
+
+    private fun configureLayout() {
+
+        if (ActivityUtil.isTvMode(this)) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.add(R.id.activity_fragment_holder, MainTvFragment())
+            transaction.commit()
+        } else {
+            Toast.makeText(this, R.string.android_tv_only_toast, Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == authRequest && resultCode == FragmentActivity.RESULT_OK) {
+            
+        }
+    }
+
+}
