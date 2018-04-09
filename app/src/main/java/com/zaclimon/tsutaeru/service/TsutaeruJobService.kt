1239a9255ad9f917f0eb9f9360ac0a3913f9cc3d
zaclimon <isaacpateau05@gmail.com>
2018-04-09 06:07:28 -0400

    TsutaeruJobService: Convert to Kotlin

---
+++ app/src/main/java/com/zaclimon/tsutaeru/service/TsutaeruJobService.kt
@@ -0,0 +1,41 @@
+package com.zaclimon.tsutaeru.service
+
+import android.app.job.JobParameters
+import android.content.Context
+import com.zaclimon.tsutaeru.R
+import com.zaclimon.tsutaeru.properties.TsutaeruChannelProperties
+import com.zaclimon.tsutaeru.util.Constants
+import com.zaclimon.xipl.properties.ChannelProperties
+import com.zaclimon.xipl.service.ProviderEpgService
+
+class TsutaeruJobService : ProviderEpgService() {
+
+    private lateinit var serviceProperties: ChannelProperties
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        serviceProperties = TsutaeruChannelProperties(getSharedPreferences(Constants.TSUTAERU_PREFERENCES, Context.MODE_PRIVATE))
+        return super.onStartJob(params)
+    }
+
+    override fun getPlaylistUrl(): String {
+        val sharedPreferences = getSharedPreferences(Constants.TSUTAERU_PREFERENCES, Context.MODE_PRIVATE)
+        val url = sharedPreferences?.getString(Constants.PROVIDER_URL_PREFERENCE, "")
+        val username = sharedPreferences?.getString(Constants.USERNAME_PREFERENCE, "")
+        val password = sharedPreferences?.getString(Constants.PASSWORD_PREFERENCE, "")
+        val streamType = sharedPreferences?.getString(Constants.STREAM_TYPE_PREFERENCE, Constants.STREAM_TYPE_HLS)
+        return getString(R.string.provider_playlist_url, url, username, password, streamType)
+    }
+
+    override fun getEpgUrl(): String {
+        val sharedPreferences = getSharedPreferences(Constants.TSUTAERU_PREFERENCES, Context.MODE_PRIVATE)
+        val url = sharedPreferences?.getString(Constants.PROVIDER_URL_PREFERENCE, "")
+        val username = sharedPreferences?.getString(Constants.USERNAME_PREFERENCE, "")
+        val password = sharedPreferences?.getString(Constants.PASSWORD_PREFERENCE, "")
+        return getString(R.string.provider_epg_url, url, username, password)
+    }
+
+    override fun getChannelProperties(): ChannelProperties {
+        return serviceProperties
+    }
+
+}
