55d322ece24bf815673c15420cdca877d73b4af5
zaclimon <isaacpateau05@gmail.com>
2018-06-10 19:26:11 -0400

    TsutaeruTvInputSetupGuidedFragment: NPE fix for Toast

--- app/src/main/java/com/zaclimon/tsutaeru/ui/tvinputsetup/TsutaeruTvInputSetupGuidedFragment.kt
+++ app/src/main/java/com/zaclimon/tsutaeru/ui/tvinputsetup/TsutaeruTvInputSetupGuidedFragment.kt
@@ -58,7 +58,7 @@ class TsutaeruTvInputSetupGuidedFragment : ChannelSetupStepSupportFragment<Tsuta
             if (resultCode == FragmentActivity.RESULT_OK) {
                 onStart()
             } else {
-                Toast.makeText(context, R.string.authentication_not_possible, Toast.LENGTH_LONG).show()
+                context ?: Toast.makeText(context, R.string.authentication_not_possible, Toast.LENGTH_LONG).show()
                 activity?.finish()
             }
         }
