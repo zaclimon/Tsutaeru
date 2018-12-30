c240c266aa9750f4214b82e343e025ebd16a8c6a
zaclimon <isaacpateau05@gmail.com>
2018-12-30 12:55:17 -0500

    xipl: Update JUnit to version 5
    - gradle-wrapper.properties: Update Gradle to 4.7
    - build.gradle: Add the android-junit5 plugin
    - build.gradle: Remove JUnit 4.12 and update to JUnit 5.3.1
    - Remove ExampleUnitTest
    - build.gradle: Define default test instance lifecycle to "PER_CLASS"
    - AvContentUtilTest: Rewrite tests for JUnit 5 as well as using more
    "Human readable" function names

--- xipl/library/src/test/java/com/zaclimon/xipl/util/AvContentUtilTest.kt
+++ xipl/library/src/test/java/com/zaclimon/xipl/util/AvContentUtilTest.kt
@@ -1,64 +1,54 @@
 package com.zaclimon.xipl.util
 
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
 
 class AvContentUtilTest {
 
-    private lateinit var validPlaylistLine: String
-    private lateinit var missingNameTagPlaylistLine: String
-    private lateinit var missingHeaderPlaylistLine: String
-    private lateinit var playlistUrl: String
-    private lateinit var contentCategory: String
-
-    @Before
-    fun init() {
-        validPlaylistLine = "#EXTINF:-1 tvg-id=\"test\" tvg-name=\"Test Channel\" tvg-logo=\"http://logo.com/abc.jpg\" group-title=\"TEST\",Test Channel"
-        missingNameTagPlaylistLine = "#EXTINF:-1 tvg-id=\"test\" tvg-name=\"\" tvg-logo=\"http://logo.com/abc.jpg\" group-title=\"TEST\",Test Channel"
-        missingHeaderPlaylistLine = "tvg-id=\"test\" tvg-name=\"Test Channel\" tvg-logo=\"http://logo.com/abc.jpg\" group-title=\"TEST\",Test Channel"
-        playlistUrl = "http://test.com:25461/live/hello/world/12345.ts"
-        contentCategory = "VOD"
-    }
+    private val validPlaylistLine = "#EXTINF:-1 tvg-id=\"test\" tvg-name=\"Test Channel\" tvg-logo=\"http://logo.com/abc.jpg\" group-title=\"TEST\",Test Channel"
+    private val missingNameTagPlaylistLine = "#EXTINF:-1 tvg-id=\"test\" tvg-name=\"\" tvg-logo=\"http://logo.com/abc.jpg\" group-title=\"TEST\",Test Channel"
+    private val missingHeaderPlaylistLine = "tvg-id=\"test\" tvg-name=\"Test Channel\" tvg-logo=\"http://logo.com/abc.jpg\" group-title=\"TEST\",Test Channel"
+    private val playlistUrl = "http://test.com:25461/live/hello/world/12345.ts"
+    private val contentCategory = "VOD"
 
     @Test
-    fun createAvContentTest_ValidLine() {
+    fun `Creates an AV content with a valid playlist line, URL and group`() {
         val avContent = createAvContent(validPlaylistLine, playlistUrl, contentCategory)
-        Assert.assertTrue(avContent != null)
+        assertTrue(avContent != null)
         // Verify that we were able to get channel's information
-        Assert.assertTrue(avContent?.id == "test".hashCode())
-        Assert.assertTrue(avContent?.title == "Test Channel")
-        Assert.assertTrue(avContent?.logo == "http://logo.com/abc.jpg")
-        Assert.assertTrue(avContent?.group == "TEST")
+        assertTrue(avContent?.id == "test".hashCode())
+        assertTrue(avContent?.title == "Test Channel")
+        assertTrue(avContent?.logo == "http://logo.com/abc.jpg")
+        assertTrue(avContent?.group == "TEST")
     }
 
     @Test
-    fun createAvContentTest_MissingNameTag() {
+    fun `Creates an AV content with missing title in the playlist line but with valid URL and group`() {
         val avContent = createAvContent(missingNameTagPlaylistLine, playlistUrl, contentCategory)
-        Assert.assertTrue(avContent != null)
+        assertTrue(avContent != null)
         // Verify that we were able to get channel's information
-        Assert.assertTrue(avContent?.id == "test".hashCode())
-        Assert.assertTrue(avContent?.title == "Test Channel")
-        Assert.assertTrue(avContent?.logo == "http://logo.com/abc.jpg")
-        Assert.assertTrue(avContent?.group == "TEST")
+        assertTrue(avContent?.id == "test".hashCode())
+        assertTrue(avContent?.title == "Test Channel")
+        assertTrue(avContent?.logo == "http://logo.com/abc.jpg")
+        assertTrue(avContent?.group == "TEST")
     }
 
     @Test
-    fun createAvContentTest_MissingHeaderLine() {
+    fun `Creates an empty AV content when the playlist line misses the M3U header`() {
         val avContent = createAvContent(missingHeaderPlaylistLine, playlistUrl, contentCategory)
-        Assert.assertTrue(avContent == null)
+        assertTrue(avContent == null)
     }
 
     @Test
-    fun createAvContentTest_EmptyPlaylistLink() {
+    fun `Creates an empty AV content when the playlist URL is blank`() {
         val avContent = createAvContent(validPlaylistLine, "", contentCategory)
-        Assert.assertTrue(avContent == null)
+        assertTrue(avContent == null)
     }
 
     @Test
-    fun createAvContentTest_EmptyContentCategory() {
+    fun `Creates an empty AV content when playlist group is blank`() {
         val avContent = createAvContent(validPlaylistLine, playlistUrl, "")
-        Assert.assertTrue(avContent == null)
+        assertTrue(avContent == null)
     }
 
 }
